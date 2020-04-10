6b00f2a3eb8f84ee17c866d0e2549d6657c7535e
loldabigboi <loldabigboi@gmail.com>
2020-04-10 18:21:33 +1200

    Added centering of drawn digit (when its fed to neural network) to digit recognition project

--- projects/digitRecognition/main.js
+++ projects/digitRecognition/main.js
@@ -6,10 +6,16 @@ let firstGuessP;
 let secondGuessP;
 let thirdGuessP;
 
+let mouseOverCanvas = false;
+
 function setup() {
 
     let canvas = createCanvas(600, 600);
+    canvas.mouseReleased(guess);
+    canvas.mouseOver(function() {mouseOverCanvas = true});
+    canvas.mouseOut(function() {mouseOverCanvas = false});
     canvas.parent("canvas-container");
+
     background(0);
 
     firstGuessP = document.getElementById("first-guess");
@@ -20,6 +26,13 @@ function setup() {
 
 function draw() {
 
+    if (mouseIsPressed && mouseOverCanvas) {
+        stroke(255);
+        strokeWeight(28);
+        line(pmouseX, pmouseY, mouseX, mouseY);
+
+        resetGuessDisplay()
+    }
 
 }
 
@@ -32,14 +45,10 @@ function resetGuessDisplay() {
 
 }
 
-function mouseDragged() {
-
-    stroke(255);
-    strokeWeight(28);
-    line(pmouseX, pmouseY, mouseX, mouseY);
-
-    resetGuessDisplay()
-
+function keyReleased() {
+    if (keyCode == 67) { // "C"
+        clearCanvas();
+    }
 }
 
 function clearCanvas() {
@@ -52,7 +61,7 @@ function guess() {
     // get pixels from canvas
     let img = get();
     img.resize(28, 28);
-    img.filter(BLUR, 0.2);
+    img.filter(BLUR, 0.1);
     img.loadPixels();
 
     // convert to inputs (grayscale)
@@ -64,6 +73,69 @@ function guess() {
             col = grayscaleIndex % 28;
         inputs.set(row, col, map(img.pixels[i], 0, 255, -0.5, 0.5));
     }
+
+    // get bounds of digit
+    let leftMost = Infinity, rightMost   = -Infinity,
+        topMost  = Infinity, bottomMost  = -Infinity;
+    for (let col = 0; col < inputs.cols; col++) {
+        for (let row = 0; row < inputs.rows; row++) {
+            if (inputs.get(row, col) > 0) {  // not completely black
+                leftMost   = Math.min(col, leftMost);
+                rightMost  = Math.max(col, rightMost);
+                topMost    = Math.min(row, topMost);
+                bottomMost = Math.max(row, bottomMost);
+            }
+        }
+    }
+    
+    if (leftMost != Infinity) {  // if leftMost is still Infinity then the canvas is blank
+        
+        // move pixels to center digit
+        let horizOffset = Math.trunc(-(leftMost - (27 - rightMost))/2),
+            vertOffset  = Math.trunc(-(topMost  - (27 - bottomMost))/2);
+        let newInputs = new Matrix(28, 28);
+        for (let col = 0; col < inputs.cols; col++) {
+            for (let row = 0; row < inputs.rows; row++) {
+                
+                let newCol = col - horizOffset,
+                    newRow = row - vertOffset;
+
+                if (newCol < 0 || newCol > 27 || newRow < 0 || newRow > 27) {
+                    newInputs.set(row, col, 0)
+                } else {
+                    newInputs.set(row, col, inputs.get(newRow, newCol));
+                }
+
+            }
+        }
+
+        let size = Math.max(rightMost - leftMost, bottomMost - topMost);
+        let desiredSize = 20;
+        let scaleRatio = desiredSize / size;
+        let scaledWidth  = 28 * scaleRatio,
+            scaledHeight = 28 * scaleRatio;
+
+        let centeredImage = createImage(28, 28);
+        newInputs.forEach((val, row, col) => {
+            centeredImage.set(row, col, color(val));
+        });
+
+        centeredImage.resize(scaledWidth, scaledHeight);
+        let scaledImage = centeredImage.get(Math.floor(scaledWidth/2  - 14),
+                                            Math.floor(scaledHeight/2 - 14));
+
+
+
+        inputs = newInputs;
+
+    }       
+    
+    // for (let i = 0; i < img.pixels.length; i+= 4) {
+    //     let grayscaleIndex = i/4,
+    //         row = Math.floor(grayscaleIndex / 28),
+    //         col = grayscaleIndex % 28;
+    //     inputs.set(row, col, map(img.pixels[i], 0, 255, -0.5, 0.5));
+    // }
 
     let outputs = neuralNetwork.predict([inputs]).map((val, index) => {
         return {
